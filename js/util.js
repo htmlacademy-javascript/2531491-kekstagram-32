b19046a5c15c2f069fa64fa1c1fef6a57f952909
Sergun Pavel <axez1@mail.ru>
2024-07-03 12:16:22 +0300

    разделение по модулям

--- js/util.js
+++ js/util.js
@@ -1,15 +1,14 @@
-const getRandomInteger = (a, b) => {
+/*const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-};
+};*/
 
-/*
 function getRandomInteger(min, max) {
   return Math.round(Math.random() * Math.abs(max - min) + min);
 }
-*/
+
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
