4aebf6e9acc61c9bf093badc44b77dda37027ea1
Keks <keks@htmlacademy.ru>
2024-07-03 13:05:00 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,5 @@
+import {infoBlock, commentBlock} from './createElemen';
+// eslint-disable-next-line no-console
+console.log(infoBlock);
+// eslint-disable-next-line no-console
+console.log(commentBlock);
